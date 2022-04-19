e4721dc4cc89319316c1bd9000f4917df1ee0f00
Maria Jose <mariajdab@gmail.com>
2022-04-19 19:45:14 -0400

    fix: remove unnecessary json

--- models/account_txn.go
+++ models/account_txn.go
@@ -3,7 +3,7 @@ package models
 import "time"
 
 type AccountTxn struct {
-	Id          uint64    `json:"id"`
-	Date        time.Time `json:"date"`
-	Transaction float64   `json:"transaction"`
+	Id          uint64
+	Date        time.Time
+	Transaction float64
 }
